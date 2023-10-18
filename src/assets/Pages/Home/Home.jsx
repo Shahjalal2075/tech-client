ac324ab8a45a54b3b9d7671d3cf8659ed04f82c6
Shahjalal2075 <shahjalal2075@gmail.com>
2023-10-18 20:44:19 +0600

    routes setup

---
+++ src/assets/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <h2 className="text-5xl text-center font-bold">Home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/assets/Shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center font-bold">Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
