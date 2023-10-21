7fc144ebe3d37fef55908b5b5cf0e59b3909311f
Shahjalal2075 <shahjalal2075@gmail.com>
2023-10-21 14:49:35 +0600

    discover section add

--- src/assets/Pages/Home/ClientReview.jsx
+++ src/assets/Pages/Home/ClientReview.jsx
@@ -2,9 +2,28 @@
 const ClientReview = () => {
     return (
         <div>
-            <h2 className="text-5xl text-center font-bold">Our Client Review</h2>
+            <h2 className="text-5xl text-center font-bold my-16">Discover</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="card shadow-xl bg-[#A2B7B5] p-6">
+                    <figure><img src="https://i.ibb.co/DMsLmrf/iphn14.png" alt="" /></figure>
+                    <div className="">
+                        <h2 className="text-2xl text-center font-bold py-2">I Phone 14</h2>
+                    </div>
+                </div>
+                <div className="card shadow-xl bg-[#A2B7B5] p-6">
+                    <figure><img src="https://i.ibb.co/VpRvV9g/iphn15.png" alt="" /></figure>
+                    <div className="">
+                        <h2 className="text-2xl text-center font-bold py-2">I Phone 15 Pink</h2>
+                    </div>
+                </div>
+                <div className="card shadow-xl bg-[#A2B7B5] p-6">
+                    <figure><img src="https://i.ibb.co/DMsLmrf/iphn14.png" alt="" /></figure>
+                    <div className="">
+                        <h2 className="text-2xl text-center font-bold py-2">I Phone 14</h2>
+                    </div>
+                </div>
+            </div>
 
-            
         </div>
     );
 };
